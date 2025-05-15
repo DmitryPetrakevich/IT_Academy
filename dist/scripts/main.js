fee47f4cb9bf1594cae18f593bcee98613ff80bf
front_academy <dmitrypetrakevich@gmail.com>
2025-05-15 10:18:25 +0700

    реализовал логику открытия//закрытия формы

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,11 +1,26 @@
 (function () {
     "use strict";
-    
     const root = document.documentElement;
 
     const navToggle = document.querySelector("#js-navToggle");
-
     navToggle.addEventListener("click", function() {
       root.classList.toggle("show-nav");
-    })
+    });
+
+    const eventpp = document.querySelector("#js-eventpp")
+    const eventOpenBtn = document.querySelector("#js-eventOpenBtn")
+
+    if(eventpp && eventOpenBtn) {
+      eventOpenBtn.addEventListener("click", function() {
+        root.classList.add("show-event-popup")
+      });
+
+      eventpp.addEventListener("click", function(event) {
+        if(event.target === this || event.target.classList.contains("js-ppCloseBtn")){
+          root.classList.remove("show-event-popup")
+        }
+      })
+    }
+
   })();
+
